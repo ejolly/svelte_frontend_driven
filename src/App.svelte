4e3b1b2efe23f16fc43af884c29e08199ef5358d
ejolly <eshin.jolly@gmail.com>
2019-06-13 17:54:15 -0400

    Added comment about how to load router.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 import { onMount } from 'svelte'
 export let router
+// Adding !!svelte-loader! prefix to the import until svelte-easyroute-webpack issues a fix. Otherwise we have to remove exclude: /node_modules from webpack.config.js to get svelte compilation working
 import RouterLink from '!!svelte-loader!svelte-easyroute-webpack/RouterLink.svelte'
 
 onMount(()=> {
